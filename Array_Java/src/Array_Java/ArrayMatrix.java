def158f9572c4f308cde48e252f01c811d0278b2
Bhimana prasanth <prasanth.theawesome@gmail.com>
2021-05-10 17:05:18 +0530

    Practice

---
+++ Array_Java/src/Array_Java/ArrayMatrix.java
@@ -0,0 +1,30 @@
+package Array_Java;
+import java.util.Scanner;
+public class ArrayMatrix {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	    int i,j;
+	    System.out.println("Please enter 9 integer numbers: ");
+		  int n=sc.nextInt();
+		  int a[][]=new int[n][n];
+		  for(i=0;i<n;i++)
+		  {
+		      for(j=0;j<n;j++)
+		      {
+			  a[i][j]=sc.nextInt();
+		      }
+		  }
+		  
+		  System.out.println("The given array is :");
+		  for(i=0;i<n;i++)
+		  {
+		  for(j=0;j<n;j++)
+		  {
+		      System.out.print(a[i][j]+" ");
+		  }
+		  System.out.print("\n");
+		  }
+	}
+}
+
+
